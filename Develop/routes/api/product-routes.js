a44d470417993beef9dfff19059d58d69249ab03
User <kyle_albright@hotmail.com>
2022-12-12 18:21:44 -0600

    initialized sequilized. Everything works!

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -22,7 +22,7 @@ try {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   try{
      // find a single product by its `id`
   // be sure to include its associated Category and Tag data
@@ -114,7 +114,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',async (req, res) => {
   try{
     // delete one product by its `id` value
     const productData = await Product.destroy({
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,7 +3,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   try{
      // be sure to include its associated Product data
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
  
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   try{
    // find a single tag by its `id`
   // be sure to include its associated Product data
@@ -34,7 +34,7 @@ router.get('/:id', (req, res) => {
 }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try{
     const tagData = await Tag.create(req.body);
@@ -44,7 +44,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try{
     tagData = await Tag.update(
@@ -64,7 +64,7 @@ router.put('/:id', (req, res) => {
 }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one tag by its `id` value
   try{
     const tagData = await Tag.destroy({
